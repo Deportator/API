18a5cb25700557cb08ef18336539c924049c8cc8
Deportator <fabrichnov87@gmail.com>
2022-05-29 23:54:02 +0300

    Initial commit

---
+++ src/main/java/ru/netology/birthday/FormDate.java
@@ -0,0 +1,11 @@
+package ru.netology.birthday;
+
+import ru.netology.domain.Post;
+
+public class FormDate {
+
+        public int day;
+        public int month;
+        public int year;
+}
+
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+import ru.netology.birthday.FormDate;
+
+public class Post {
+
+    public FormDate birthday;
+    public String name; // поле для имени
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
